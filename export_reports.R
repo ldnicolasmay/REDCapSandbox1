083f432193d2968dea21dcee8a977a86f34380b8
L. D. Nicolas May <LDNicolasMay@gmail.com>
2018-02-27 16:12:17 -0500

    Verify redcapAPI not working; use only RCurl + jsonlite packages

--- export_reports.R
+++ export_reports.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 source("config.R") # contains API URL and API token
-library(redcapAPI)
 library(RCurl)
 library(jsonlite)
 
@@ -26,27 +25,33 @@ result <- postForm(
   rawOrLabel = 'label',
   rawOrLabelHeaders = 'label',
   exportCheckboxLabel = 'false',
-  returnFormat = 'json'
+  returnFormat = 'json',
+  config = c(ssl_verifypeer = TRUE, ssl_verifyhost = TRUE)
 )
 result_json <- fromJSON(result)
 print(result_json)
 
-rcon <- redcapConnection(url = api_url, token = api_token)
+
+
+# library(redcapAPI)
+
+# Only getting an error using the redcapAPI
+# I'm just going to stick to using RCurl + jsonlite
+# rcon <- redcapConnection(url = api_url, token = api_token)
 # report <- exportReports(rcon = rcon, report_id = '1680') # Doesn't work
 # report <- exportReports(rcon = rcon, report_id = '1680', 
 #                         factors = TRUE, labels = TRUE, 
 #                         dates = TRUE, checkboxLabels = FALSE)
+# exportProjectInformation(rcon)
+# exportVersion(rcon)
 
-exportProjectInformation(rcon)
-exportVersion(rcon)
-
-if (FALSE){
-  # exportBundle(rcon = rcon)
-  # report <- exportReports(rcon = rcon, report_id = 1680, 
-  #                         factors = TRUE, labels = TRUE, dates = TRUE)
-  # save(report, file = "report.Rdata")
-}
-print(report)
-class(report)
+# if (FALSE){
+#   exportBundle(rcon = rcon)
+#   report <- exportReports(rcon = rcon, report_id = 1680,
+#                           factors = TRUE, labels = TRUE, dates = TRUE)
+#   save(report, file = "report.Rdata")
+# }
+# print(report)
+# class(report)
 
 
